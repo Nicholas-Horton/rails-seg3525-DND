5e558078724e8850720521231f2b7bb0ac334fde
Julien <julien.mageau@live.ca>
2015-07-15 17:05:38 -0400

    Changed table row links to use divs with table display

--- app/assets/javascripts/link_row.js
+++
@@ -1,8 +0,0 @@
-// JQuery for placing a link onto table rows with the class "clickableRow".
-// Upon clicking these rows, the browser will follow the data-href link.
-
-jQuery(document).ready(function($) {
-    $(".clickableRow").click(function() {
-        window.document.location = $(this).data("href");
-    });
-});
