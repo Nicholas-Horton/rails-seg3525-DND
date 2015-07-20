da70be768925ea5ed3ece162c4a12e80defac17e
Julien <julien.mageau@live.ca>
2015-07-20 14:34:53 -0400

    Cleaned up controllers

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,64 +1,40 @@
 class CreaturesController < ApplicationController
   before_action :set_creature, only: [:show, :edit, :update, :destroy]
 
-  # GET /creatures
-  # GET /creatures.json
   def index
     @creatures = Creature.all
   end
 
-  # GET /creatures/1
-  # GET /creatures/1.json
   def show
   end
 
-  # GET /creatures/new
   def new
     @creature = Creature.new
   end
 
-  # GET /creatures/1/edit
   def edit
   end
 
-  # POST /creatures
-  # POST /creatures.json
   def create
     @creature = Creature.new(creature_params)
-
-    respond_to do |format|
-      if @creature.save
-        format.html { redirect_to @creature, notice: 'Creature was successfully created.' }
-        format.json { render :show, status: :created, location: @creature }
-      else
-        format.html { render :new }
-        format.json { render json: @creature.errors, status: :unprocessable_entity }
-      end
+    if @creature.save
+      redirect_to @creature, notice: 'Creature was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /creatures/1
-  # PATCH/PUT /creatures/1.json
   def update
-    respond_to do |format|
-      if @creature.update(creature_params)
-        format.html { redirect_to @creature, notice: 'Creature was successfully updated.' }
-        format.json { render :show, status: :ok, location: @creature }
-      else
-        format.html { render :edit }
-        format.json { render json: @creature.errors, status: :unprocessable_entity }
-      end
+    if @creature.update(creature_params)
+      redirect_to @creature, notice: 'Creature was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /creatures/1
-  # DELETE /creatures/1.json
   def destroy
     @creature.destroy
-    respond_to do |format|
-      format.html { redirect_to creatures_url, notice: 'Creature was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to creatures_url, notice: 'Creature was successfully destroyed.'
   end
 
   private
--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -1,64 +1,40 @@
 class SpellsController < ApplicationController
   before_action :set_spell, only: [:show, :edit, :update, :destroy]
 
-  # GET /spells
-  # GET /spells.json
   def index
     @spells = Spell.all
   end
 
-  # GET /spells/1
-  # GET /spells/1.json
   def show
   end
 
-  # GET /spells/new
   def new
     @spell = Spell.new
   end
 
-  # GET /spells/1/edit
   def edit
   end
 
-  # POST /spells
-  # POST /spells.json
   def create
     @spell = Spell.new(spell_params)
-
-    respond_to do |format|
-      if @spell.save
-        format.html { redirect_to @spell, notice: 'Spell was successfully created.' }
-        format.json { render :show, status: :created, location: @spell }
-      else
-        format.html { render :new }
-        format.json { render json: @spell.errors, status: :unprocessable_entity }
-      end
+    if @spell.save
+      redirect_to @spell, notice: 'Spell was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /spells/1
-  # PATCH/PUT /spells/1.json
   def update
-    respond_to do |format|
-      if @spell.update(spell_params)
-        format.html { redirect_to @spell, notice: 'Spell was successfully updated.' }
-        format.json { render :show, status: :ok, location: @spell }
-      else
-        format.html { render :edit }
-        format.json { render json: @spell.errors, status: :unprocessable_entity }
-      end
+    if @spell.update(spell_params)
+      redirect_to @spell, notice: 'Spell was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /spells/1
-  # DELETE /spells/1.json
   def destroy
     @spell.destroy
-    respond_to do |format|
-      format.html { redirect_to spells_url, notice: 'Spell was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to spells_url, notice: 'Spell was successfully destroyed.'
   end
 
   private
